be6b35893eb54c67759b8360e789384a5d94c7a4
Angie Mercier <angmerc97@gmail.com>
2014-12-02 12:34:46 -0600

    MovieSelector Form
    project

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MovieSelector
+{
+    partial class movieForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(movieForm));
+            this.listBoxMovies = new System.Windows.Forms.ListBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lblAll = new System.Windows.Forms.Label();
+            this.lblMovie = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxMovies
+            // 
+            this.listBoxMovies.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxMovies.FormattingEnabled = true;
+            this.listBoxMovies.ItemHeight = 15;
+            this.listBoxMovies.Items.AddRange(new object[] {
+            "Dumb and Dumber To",
+            "Big Hero 6",
+            "Interstellar",
+            "Gone Girl"});
+            this.listBoxMovies.Location = new System.Drawing.Point(12, 46);
+            this.listBoxMovies.Name = "listBoxMovies";
+            this.listBoxMovies.Size = new System.Drawing.Size(259, 64);
+            this.listBoxMovies.TabIndex = 0;
+            this.listBoxMovies.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBoxMovies.MouseLeave += new System.EventHandler(this.listBox1_MouseLeave);
+            this.listBoxMovies.MouseHover += new System.EventHandler(this.listBox1_MouseHover);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(23, 21);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(241, 15);
+            this.lblInstructions.TabIndex = 1;
+            this.lblInstructions.Text = "Select a movie title to see starting times:";
+            // 
+            // lblAll
+            // 
+            this.lblAll.AutoSize = true;
+            this.lblAll.Font = new System.Drawing.Font("Corbel", 9.75F);
+            this.lblAll.Location = new System.Drawing.Point(12, 113);
+            this.lblAll.Name = "lblAll";
+            this.lblAll.Size = new System.Drawing.Size(259, 60);
+            this.lblAll.TabIndex = 2;
+            this.lblAll.Text = resources.GetString("lblAll.Text");
+            this.lblAll.Visible = false;
+            // 
+            // lblMovie
+            // 
+            this.lblMovie.AutoSize = true;
+            this.lblMovie.Location = new System.Drawing.Point(12, 125);
+            this.lblMovie.Name = "lblMovie";
+            this.lblMovie.Size = new System.Drawing.Size(35, 13);
+            this.lblMovie.TabIndex = 3;
+            this.lblMovie.Text = "movie";
+            this.lblMovie.Visible = false;
+            this.lblMovie.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lblMovie_MouseClick);
+            // 
+            // movieForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 182);
+            this.Controls.Add(this.lblMovie);
+            this.Controls.Add(this.lblAll);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.listBoxMovies);
+            this.Name = "movieForm";
+            this.Text = "Movie Selector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxMovies;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label lblAll;
+        private System.Windows.Forms.Label lblMovie;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieSelector
+{
+    public partial class movieForm : Form
+    {
+        public movieForm()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_MouseHover(object sender, EventArgs e)
+        {
+            lblAll.Visible = true;
+        }
+
+        private void listBox1_MouseLeave(object sender, EventArgs e)
+        {
+            lblAll.Visible = false;
+        }
+
+        private void lblMovie_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selMovie = listBoxMovies.SelectedItem.ToString();
+
+            if (selMovie == "Dumb and Dumber To")
+            {
+                lblAll.Visible = false;
+                lblMovie.Visible = true;
+                lblMovie.Text = "Start Times for Dumb and Dumber To are:\n  8:00  |  10:40  |  1:20";
+                this.BackColor = System.Drawing.Color.White;
+            }
+            else if (selMovie == "Big Hero 6")
+            {
+                lblAll.Visible = false;
+                lblMovie.Visible = true;
+                lblMovie.Text = "Start Times for Big Hero 6 are:\n  10:45  |  12:30  |  1:25  |  3:10  |  4:04";
+                this.BackColor = System.Drawing.Color.Wheat;
+            }
+            else if (selMovie == "Interstellar")
+            {
+                lblAll.Visible = false;
+                lblMovie.Visible = true;
+                lblMovie.Text = "Start Times for Interstellar are:\n  11:30  |  1:30  |  3:15  |  5:10  |  6:00";
+                this.BackColor = System.Drawing.Color.MintCream;
+            }
+            else
+            {
+                lblAll.Visible = false;
+                lblMovie.Visible = true;
+                lblMovie.Text = "Start Times for Gone Girl are:\n  11:20  |  1:50  |  4:25  |  7:10  |  9:45";
+                this.BackColor = System.Drawing.Color.MistyRose;
+            }
+
+        }
+    }
+}
